0391a0733de03ef26f31d7345715bde85073a385
andrewDzokwom <andrewa441@gmail.com>
2025-01-19 10:08:54 +0100

    install dotenv and set it  up

--- mongo.js
+++ mongo.js
@@ -1,1 +1,54 @@
-const url = `mongodb+srv://phonebook:<db_password>@cluster0.xbvxt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const  mongoose = require('mongoose')
+
+
+
+// if (process.argv.length < 5){
+//     console.log('provide the arguments un the following order: node mongo.js <password> <name> <number>')
+//     process.exit(1)
+// }
+
+const password = process.argv[2]
+ mongoose.set('strictQuery', false)
+
+const url = `mongodb+srv://phonebook:${password}@cluster0.xbvxt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+//connect to db
+mongoose.connect(url)
+
+// create the schema
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+// create the model
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5){
+    // create new person 
+    const person =  new Person({
+        name:process.argv[3],
+        number: process.argv[4]
+    })
+
+
+    person.save().then((person)=>{
+        console.log(`added ${person.name} number ${person.number} to the phonebook`)
+        //close the connection
+        mongoose.connection.close()
+    })
+
+}
+
+
+
+if (process.argv.length === 3){
+    Person.find({}).then(result =>{
+        console.log('phonebook:')
+        result.forEach(person =>{
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
+
