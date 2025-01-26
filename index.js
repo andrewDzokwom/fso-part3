c9f87625ef4c6e753444160b394d83367135c41a
andrewDzokwom <andrewa441@gmail.com>
2025-01-26 11:16:04 +0100

    solve issue of communication with the database, handling of one person contact, many contacts, error handling when can't reach db or wrong input from user

--- index.js
+++ index.js
@@ -11,41 +11,71 @@ app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 
-// let persons = [
-//     { 
-//       "id": "1",
-//       "name": "Arto Hellas", 
-//       "number": "040-123456"
-//     },
-//     { 
-//       "id": "2",
-//       "name": "Ada Lovelace", 
-//       "number": "39-44-5323523"
-//     },
-//     { 
-//       "id": "3",
-//       "name": "Dan Abramov", 
-//       "number": "12-43-234345"
-//     },
-//     { 
-//       "id": "4",
-//       "name": "Mary Poppendieck", 
-//       "number": "39-23-6423122"
-//     }
-// ]
-
 app.get("/", (req, res)=>{
     res.send("<h1>This is the HomePage</h1>")
 })
-
+ 
+// get list of contacts 
 app.get("/api/persons", (req, res) =>{
     Person.find({}).then(persons => {
         res.json(persons)
+    }).catch(error => {
+        console.log(error)
+        res.status(400).json({
+            error: "unable to reach "
+        })
     })
 })
 
-function getTotalPersons(){
-    return persons.length
+// add contact to the list
+app.post("/api/persons",  (req, res)=> {
+    const {name, number}= req.body
+    // check if name and number are both empty
+    if (!name.trim() && !number.trim()){
+        return   res.status(404).json({
+                error: "name and number is missing"
+            })
+    }
+    //check if only  number is empty
+    if(!number.trim()){
+        return   res.status(404).json({
+                error: "number is missing"
+            })
+    }
+    // check if only name is empty
+    if (!name.trim()){
+        return res.status(404).json({
+            error: "name is missing"
+        })
+    }
+
+    // check for  duplication or add if there is any (name only)
+    Person.find({name})
+        .then(returnedPerson => {
+
+        const isEmpty = returnedPerson.length === 0
+        if (!isEmpty){
+            const errorMessage = {
+                error: "name must be unique"
+            }
+            res.status(400).json(errorMessage)
+        }else{
+            const  newPerson = new Person({ name, number})
+            newPerson.save().then(savedPerson =>{
+                res.json(savedPerson)
+            })
+            
+        }
+    }).catch(error => {
+        console.log(error)
+        res.status(400).json({
+            error: "unable to reach "
+        })
+    })
+
+async function getTotalPersons(){
+    const persons  = await Person.find({}).then(persons => persons)
+    return await persons.length
 }
 
 app.get("/info", (req, res)=>{
@@ -55,12 +85,22 @@ app.get("/info", (req, res)=>{
     res.send(`<p>${text}</p><p>${timeNow}</p>`)
 })
 app.get("/api/persons/:id", (req, res)=>{
-    const personId = req.params.id
-    const person = persons.find(person => person.id === personId)
-    if (person){
-        res.json(person)
-    }
-    res.status(404).end()
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person){
+                res.json(person)
+            }else{
+                res.status(404).json({
+                    error: "person not found / could have also used end() method without a message."
+                })
+            }
+        })
+        .catch(error =>{
+            console.log(error)
+            res.status(404).json({
+                error: "malformatted id"
+            })
+        })
 })
 
 app.delete("/api/persons/:id", (req, res)=>{
@@ -92,28 +132,28 @@ app.use(morgan(function (tokens, req, res) {
     ].join(' ')
   }))
 
-app.post("/api/persons",  (req, res)=> {
-    const body= req.body
+
     
-    if (!body.name || !body.number){
-        const errorMessage = {
-            error: "name or number is empty"
-        }
-        res.status(400).json(errorMessage)
-    }else if(persons.find(person => person.name === body.name)){
-        const errorMessage = {
-            error: "name must be unique"
-        }
-        res.status(400).json(errorMessage)
-    }else{
-        const  person = {
-            id: String(generateId()),
-            name: body.name,
-            number: body.number
-        }
-        persons = persons.concat(person)
-        res.json(person)
-    }
+    // if (!body.name.trim() || !body.number.trim()){
+    //     const errorMessage = {
+    //         error: "name or number is empty"
+    //     }
+    //     res.status(400).json(errorMessage)
+    // }else if(Person.find({name: body.name}.then(person => person.length > 0))){
+    //     const errorMessage = {
+    //         error: "name must be unique"
+    //     }
+    //     res.status(400).json(errorMessage)
+    // }else{
+    //     const  person = new Person({
+    //         name: body.name,
+    //         number: body.number
+    //     })
+        
+    //     person.save().then(savedPerson =>{
+    //         res.json(person)
+    //     })
+    // }
 })
 
 app.listen(PORT, ()=>{
