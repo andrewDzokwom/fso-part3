7757974c286a411c2c248c1dbf328f9bbd6fda66
andrewDzokwom <andrewa441@gmail.com>
2025-01-31 19:53:41 +0100

    refactor person model for improved readability and consistency

--- models/person.js
+++ models/person.js
@@ -5,40 +5,40 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to database...')
 mongoose.connect(url)
-    .then(result =>{
-        console.log('connectedd to database')
-    })
-    .catch(error =>{
-        console.log('failed to connect to database', error.message)
-    })
+  .then(() => {
+    console.log('connectedd to database')
+  })
+  .catch(error => {
+    console.log('failed to connect to database', error.message)
+  })
 
 const isValidPhoneNumber = (phoneNumber) => {
-    var pattern = /^\d{2,3}-\d+$/
-    return pattern.test(phoneNumber)
+  var pattern = /^\d{2,3}-\d+$/
+  return pattern.test(phoneNumber)
 }
-      
+
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: [3, 'Too short name'],
-        required: true,
-        unique: true,
-      },
-    number: {
-        type: String,
-        minLength: [8, 'Too short number'],
-        required: true,
-        validate: [isValidPhoneNumber, 'Not a valid phone number'],
-      },
+  name: {
+    type: String,
+    minLength: [3, 'Too short name'],
+    required: true,
+    unique: true,
+  },
+  number: {
+    type: String,
+    minLength: [8, 'Too short number'],
+    required: true,
+    validate: [isValidPhoneNumber, 'Not a valid phone number'],
+  },
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) =>{
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
-module.exports = mongoose.model("Person", personSchema)
+module.exports = mongoose.model('Person', personSchema)
